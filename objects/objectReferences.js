e719f97d3e1a137a676e3626f52c5a1980d08118
Hung Nguyen <hqnfinance@gmail.com>
2019-05-22 11:55:04 -0700

    Add Expense Tracker Functions

--- objects/objectReferences.js
+++ objects/objectReferences.js
@@ -11,8 +11,24 @@ otherAccount.income = 1000; //now gives it 1000 to income to myAccount as well
 let addExpense = function(account, amount) {
   // account = {} this assigns a new value to the account object, so it no longer takes the properties and values of myAccount
   account.expenses = account.expenses + amount; //you have to manipulate the property in order to inherit the properties and valuesf rom myAccount
-  console.log(account);
 }
 
-addExpense(myAccount, 2.50);
-console.log(myAccount);
+let addIncome = function(account, amount) {
+  account.income = account.income + amount;
+};
+
+let resetAccount = function(account) {
+  account.expenses = 0;
+  account.income = 0;
+}
+
+let getAccountSummary = function(account) {
+  summary = `${account.name} has a balance of ${account.income - account.expenses}`;
+  return summary;
+}
+
+addExpense(myAccount, 2000);
+addIncome(myAccount, 5000);
+console.log(getAccountSummary(myAccount));
+resetAccount(myAccount);
+console.log(getAccountSummary(myAccount));
