1b3c85deab00940aa8bc72c99c264fc4300e8fe8
Hung Nguyen <hqnfinance@gmail.com>
2019-05-28 13:34:06 -0700

    Search Array Fix

--- arrays/searchArray.js
+++ arrays/searchArray.js
@@ -1,16 +1,24 @@
-// const notes = ['note 1', 'note 2', 'note 3'];
-//
-// notes.forEach((items, index)=> {
-//   console.log(index);
-//   console.log(items);
-// });
+//empty object logs -1 because it doesnt exist.
+console.log(note.indexOf({}));
+//when comparing ojects, they are not equal unless they're the EXACT same object in memory
+console.log({} === {}); //this logs false.
+let someOject = {};
+let otherObject = someOject;
+console.log(someOject === otherObject) //logs True
 
-// console.log(notes);
-//
-// //give the position of this; if it doesn't exist, we get -1;
-// console.log(notes.indexOf('note 2'));
-// //this will log -1 because it is case sensitive
-// console.log(notes.indexOf('Note 2'));
+const notes = ['note 1', 'note 2', 'note 3'];
+
+notes.forEach((items, index)=> {
+  console.log(index);
+  console.log(items);
+});
+
+console.log(notes);
+
+//give the position of this; if it doesn't exist, we get -1;
+console.log(notes.indexOf('note 2'));
+//this will log -1 because it is case sensitive
+console.log(notes.indexOf('Note 2'));
 
 const note = [{}, {
   title: 'My next trip',
@@ -25,13 +33,7 @@ const note = [{}, {
 //indexOf
 // console.log(note);
 console.log(note.length);
-// //empty object logs -1 because it doesnt exist.
-// console.log(note.indexOf({}));
-//when comparing ojects, they are not equal unless they're the EXACT same object in memory
-// console.log({} === {}); //this logs false.
-// let someOject = {};
-// let otherObject = someOject;
-// console.log(someOject === otherObject) //logs True
+
 
 // findIndex goal os findIndex is to exactly find the index. With this, we return true or false.
 const index = note.findIndex(function (item, index){
